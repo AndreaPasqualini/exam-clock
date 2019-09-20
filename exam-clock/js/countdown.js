d6388a0643a5c6e8f99ddc4de2ed6dd13d5d2a3c
Andrea Pasqualini <apsql@users.noreply.github.com>
2019-09-20 20:55:52 +0200

    Change approach to code countdown

--- exam-clock/js/countdown.js
+++ exam-clock/js/countdown.js
@@ -1,28 +1,17 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2019 15:37:25").getTime();
+function format(minutes, seconds) {
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  display.textContent = minutes + ':' + seconds;
+};
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+function countdown() {
+  var timer = new CountDownTimer();
+  var timeObj = CountDownTimer.parse();
 
-  // Get todays date and time
-  var now = new Date().getTime();
+  format(timeObj.minutes, timeObj.seconds);
+  timer.onTick(format);
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Output the result in an element with id="demo"
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  // If the count down is over, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "EXPIRED";
-  }
-}, 1000);
+  document.getElementById('cdStartButton').addEventListener('click', function () {
+    timer.start();
+  });
+};
